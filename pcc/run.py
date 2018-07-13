9d935fbe57e92e318c2b3873fd9665c7036af9a4
DearEirs <369574757@qq.com>
2018-07-13 22:33:46 +0800

    Update run.py

--- pcc/run.py
+++ pcc/run.py
@@ -18,7 +18,6 @@ func_dict = {
 app = Sanic()
 
 
-
 @app.route("/pcc", methods=['GET'])
 async def dispatch(request):
     '''根据提交的参数分发请求'''
@@ -29,26 +28,25 @@ async def dispatch(request):
             "error_message": "未能提供正确的参数, 请检查参数后重新发起请求"
         }
         return response.json(result)
-
+    
+    oid = request.args.get('oid')
     if action == 'list':
         uid = request.args.get('uid')
-        oid = request.args.get('oid')
         cursor =  request.args.get('cursor')
         page_size = request.args.get('page_size')
         is_friend = request.args.get('is_friend')
 
         action = func_dict[action]
         result = await action(uid, oid, cursor, page_size, is_friend)
+        
     elif action == 'like' or action == 'is_like':
         uid = request.args.get('uid')
-        oid = request.args.get('oid')
         action = func_dict[action]
         result = await action(uid, oid)
+        
     elif action == 'count':
-        oid = request.args.get('oid')
         action = func_dict[action]
         result = await action(oid)
-
 
     return response.json(result)
 

22dc72ea7161802f2589c028d5a7944b83f03114
root <root@localhost.localdomain>
2018-07-14 22:03:50 +0800

    封装异步mysql

--- pcc/db.py
+++ pcc/db.py
@@ -1,19 +1,14 @@
 import aiomysql
-from settings import MYSQL_CONFIG as m
+from settings import MYSQL_CONFIG as conf
 
 
 class Mysql:
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, "_instance"):
-            cls._instance = object.__new__(cls, *args, **kwargs)
-        return cls._instance
-
     def __init__(self,
-                 host=m['host'],
-                 user=m['user'],
-                 password=m['password'],
-                 port=m['port'],
-                 db=m['db']):
+                 host=conf['host'],
+                 user=conf['username'],
+                 password=conf['password'],
+                 port=conf['port'],
+                 db=conf['db']):
         self.host = host
         self.user = user
         self.password = password
@@ -28,10 +23,7 @@ class Mysql:
                                                db=self.db)
         return self.pool
 
-    async def execute(self, sql):
+    async def conn(self):
         if not hasattr(self, "pool"):
             await self.create_pool()
-        async with self.pool.get() as conn:
-            async with conn.cursor() as cur:
-                result = await cur.execute(sql)
-                return result
+        return self.pool.get()
